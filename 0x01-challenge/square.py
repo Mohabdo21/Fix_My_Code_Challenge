5c7def9fe60898548d21ceeb9607d8d15854c451
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-14 03:12:15 +0400

    Fix square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -21,9 +21,9 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """Calculating PermiterOfMySquare"""
-        return (self.width + self.height) * 2
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """Repr the class"""
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
